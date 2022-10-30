c20b85bc8f7a90c29055c8c3e459f6bce04dbe5a
Justin <zeh003@ucsd.edu>
2022-10-30 02:26:19 -0700

    Update img

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -6,7 +6,6 @@ function init() {
   var hornSelect = document.getElementById('horn-select');
   var imgSource = document.querySelectorAll('img');
   hornSelect.addEventListener('change', function() {
-    imgSource[0]='/assets/images/car-horn.svg';
-    console.log(imgSource[0]);
+    imgSource[0].src = 'assets/images/air-horn.svg';
   })
 }
