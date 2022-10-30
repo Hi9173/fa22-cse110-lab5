359577688c55838b2d05b5fef43e34464bc47000
Justin <zeh003@ucsd.edu>
2022-10-30 03:00:11 -0700

    Update audio

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -5,7 +5,9 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
   var hornSelect = document.getElementById('horn-select');
   var imgSource = document.querySelectorAll('img');
+  var audioSource = document.querySelector('audio');
   hornSelect.addEventListener('change', (event) => {
     imgSource[0].src = 'assets/images/' + event.target.value + '.svg';
+    audioSource.src = 'assets/audio/' + event.target.value + '.mp3';
   })
 }
