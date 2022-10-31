a651e58f74e60e07f5164f36d7bbf7ad868206e5
Justin <zeh003@ucsd.edu>
2022-10-30 23:20:18 -0700

    Update explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,20 +3,26 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
+  const synth = window.speechSynthesis;
+  const inputTxt = document.querySelector('#text-to-speak');
+  const voiceSelect = document.querySelector('select');
+  const voices = [];
+  const img = document.querySelector('img');
+
   function populateVoiceList() {
-    if (typeof speechSynthesis === 'undefined') {
-      return;
-    }
-    const voices = speechSynthesis.getVoices();
-    for (let i = 0; i < voices.length; i++) {
+    voices = synth.getVoices();
+  
+    for (let i = 0; i < voices.length ; i++) {
       const option = document.createElement('option');
       option.textContent = `${voices[i].name} (${voices[i].lang})`;
+  
       if (voices[i].default) {
         option.textContent += ' — DEFAULT';
       }
+  
       option.setAttribute('data-lang', voices[i].lang);
       option.setAttribute('data-name', voices[i].name);
-      document.getElementById("voice-select").appendChild(option);
+      voiceSelect.appendChild(option);
     }
   }
   populateVoiceList();
@@ -25,14 +31,20 @@ function init() {
   }
 
   const button = document.querySelector('button');
-  const img = document.querySelector('img');
   button.addEventListener('click', function() {
-    const input = document.getElementById('text-to-speak');
-    const utterThis = new SpeechSynthesisUtterance(input.value);
-    img.src = 'assets/images/smiling-open.png';
-    speechSynthesis.speak(utterThis);
-    img.src = 'assets/images/smiling.png';
-    input.blur();
-  })
+    const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+    const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    for (let i = 0; i < voices.length ; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
 
+    synth.speak(utterThis);
+    img.src = 'assets/images/smiling-open.png';
+
+    utterThis.addEventListener('end', function() {
+      img.src = 'assets/images/smiling.png';
+    })
+  })
 }
