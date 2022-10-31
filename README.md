3ad71cc2a8d572803e9ca0023f285a5a834e4715
Justin <zeh003@ucsd.edu>
2022-10-30 22:40:24 -0700

    Update explore

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 Justin He
 
-Link to expose:https://hi9173.github.io/fa22-cse110-lab5/expose.html
+Link to expose.html:https://hi9173.github.io/fa22-cse110-lab5/expose.html
+
+Link to explore.html:
+
+Link to CI:https://github.com/Hi9173/introduction-to-github
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -7,25 +7,25 @@ function init() {
     if (typeof speechSynthesis === 'undefined') {
       return;
     }
-  
     const voices = speechSynthesis.getVoices();
-  
     for (let i = 0; i < voices.length; i++) {
       const option = document.createElement('option');
       option.textContent = `${voices[i].name} (${voices[i].lang})`;
-  
       if (voices[i].default) {
         option.textContent += ' — DEFAULT';
       }
-  
       option.setAttribute('data-lang', voices[i].lang);
       option.setAttribute('data-name', voices[i].name);
       document.getElementById("voice-select").appendChild(option);
     }
   }
-  
   populateVoiceList();
-  if (typeof speechSynthesis !== 'undefined' && speechSynthesis.onvoiceschanged !== undefined) {
-    speechSynthesis.onvoiceschanged = populateVoiceList;
-  }
+
+  const button = document.querySelector('button');
+  button.addEventListener('click', function() {
+    const input = document.getElementById('text-to-speak');
+    const utterThis = new SpeechSynthesisUtterance(input.value);
+  speechSynthesis.speak(utterThis);
+  })
+
 }
